f3ec2e2f4a389367ca7c15a5cd79dee29eeb5bac
Kimaru thagana <macbuntu@localhost.localdomain>
2021-05-23 21:27:11 +0300

    build transaction generator

--- generator/app.py
+++ generator/app.py
@@ -1,10 +1,14 @@
-import os, time
+import os, time, json
 from kafka import KafkaProducer
+
+from transactions import create_random_transaction
 
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
 producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
+
 while True:
-    message = ""  # TODO
+    transaction = create_random_transaction()
+    message = json.dumps(transaction)
     # Kafka messages are sent in bytes
     producer.send("queueing.transactions", value=message.encode())
     time.sleep(1)  
